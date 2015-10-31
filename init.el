766400cac9bbed3d5a4b3918c548170ae05651ce
Raoul Felix <raoul@talkdesk.com>
2015-10-31 15:30:10 +0000

    Bare bones emacs configuration with basic evil-mode
    Following the guide at
    http://nathantypanski.com/blog/2014-08-03-a-vim-like-emacs-config.html

---
+++ init.el
@@ -0,0 +1,13 @@
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(require 'evil)
+(evil-mode 1)
+
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
