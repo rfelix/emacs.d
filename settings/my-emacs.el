1414f7d93b645828cca3c1b7ebce2f58465f2f1a
Raoul Felix <raoul@talkdesk.com>
2015-11-05 23:54:23 +0000

    Adjust Helm's appearance and some keybindings (be more VIM like)
    - As it was Helm would seemingly appear randomly, what is was
      doing is choosing the available window that was next to the
      current one you were in which was always different. With this
      change it will always appear at the bottom of the current buffer
    - Adjust some keybindings to be more VIM like

--- settings/my-emacs.el
+++ settings/my-emacs.el
@@ -18,7 +18,7 @@
     (setq helm-bookmark-show-location t
           helm-display-header-line nil
           helm-echo-input-in-header-line t)
-    ;; Use fuzzy-matching
+    ; Use fuzzy-matching
     (setq helm-M-x-fuzzy-match t
           helm-apropos-fuzzy-match t
           helm-file-cache-fuzzy-match t
@@ -33,16 +33,27 @@
     (require 'helm-config)
     ;; Enable helm with various normal Emacs commands
     (helm-mode 1)
+    ;; Only have help open up at the bottom of the current window rather
+    ;; than always show up in the "other" window
+    (setq helm-split-window-in-side-p t)
+    ;; And have it auto-resize the window based on items, but not too big
+    (helm-autoresize-mode 1)
+    (setq helm-autoresize-max-height 40)
     ;; Use vim-like keys for navigation, taken from Spacemacs
     (define-key helm-map (kbd "C-j") 'helm-next-line)
     (define-key helm-map (kbd "C-k") 'helm-previous-line)
-    (define-key helm-map (kbd "C-h") 'helm-next-source)
+    (define-key helm-map (kbd "C-h") 'helm-previous-source)
+    (define-key helm-map (kbd "C-l") 'helm-next-source)
     (define-key helm-map (kbd "C-S-h") 'describe-key)
-    (define-key helm-map (kbd "C-l") (kbd "RET"))
     (dolist (keymap (list helm-find-files-map helm-read-file-map))
       (define-key keymap (kbd "C-l") 'helm-execute-persistent-action)
       (define-key keymap (kbd "C-h") 'helm-find-files-up-one-level)
       (define-key keymap (kbd "C-S-h") 'describe-key))
+    ;; Experimental
+    (define-key helm-map (kbd "C-<SPC>") 'helm-toggle-visible-mark)
+    (define-key helm-map (kbd "C-v") 'helm-execute-persistent-action)
+    (define-key helm-map (kbd "C-u") 'helm-beginning-of-buffer)
+    (define-key helm-map (kbd "C-d") 'helm-end-of-buffer)
     ;; Helm displays a big caption at the top which is a bit ugly and
     ;; distracting, so lets try remoe it as best we can
     (defun my-helm-toggle-header-line ()
@@ -51,4 +62,11 @@
         (set-face-attribute 'helm-source-header nil :height 1.0)))
 
     (add-hook 'helm-before-initialize-hook 'my-helm-toggle-header-line)
+    ;; Switch the command execution to use Helm instead as besides giving
+    ;; fuzzy search capability it also displays the corresponding keybinding
+    (global-set-key (kbd "M-x") 'helm-M-x) 
+    ;; Allow easy switching between buffers or recent files
+    (global-set-key (kbd "C-x b") 'helm-mini)
+    ;; Better find-files
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
     ))
